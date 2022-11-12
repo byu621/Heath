594ce1ee6db1fa4ab93f8d486895167ffc998bac
Benjamin-Yu <ben.yu2@datacom.co.nz>
2022-11-13 12:18:21 +1300

    resize screen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,9 +35,10 @@ namespace Heath
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(323, 131);
+            this.button1.Location = new System.Drawing.Point(155, 98);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.Size = new System.Drawing.Size(137, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Open Branch";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,9 +46,10 @@ namespace Heath
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(323, 183);
+            this.button2.Location = new System.Drawing.Point(155, 189);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 23);
+            this.button2.Size = new System.Drawing.Size(137, 28);
             this.button2.TabIndex = 1;
             this.button2.Text = "Create PR";
             this.button2.UseVisualStyleBackColor = true;
@@ -55,11 +57,12 @@ namespace Heath
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(460, 554);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Heath";
             this.ResumeLayout(false);
